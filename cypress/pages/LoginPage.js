21552c3b4fd6956ccc069a0efbc238c08b94dcb2
haris124421 <haris.fastian12@gmail.com>
2022-09-26 12:42:49 +0500

    created OLX BH Sanity Project with login case

---
+++ cypress/pages/LoginPage.js
@@ -0,0 +1,30 @@
+import { cyan } from "colorette";
+import { eq } from "lodash";
+
+export class LoginPage {
+
+    navigate(){
+        cy.visit('https://olx.com.bh/en')
+    }
+
+    login(userName, password,type,i){
+        cy.contains("Login").click();
+        cy.get('._5fd7b300.c6ce5164._31a14546._42207ab4').eq(i).click();
+        cy.get(type).type(userName);
+        cy.contains("Next").click();
+        cy.get('#password').type(password);
+        cy.contains("Log in").click();
+    }
+
+    validateLogin(){
+        cy.get('._0559c5ad').should('be.visible');
+    }
+
+    invalidPasswordLogin(){
+        cy.get('._8918c0a8').should('be.visible');
+    }
+
+    loginbox(){
+        cy.get('._470ef52d._9bb11e5f').should('be.visible');
+    }
+}
