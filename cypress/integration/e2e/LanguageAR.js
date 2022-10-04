2b19a647fb1eff043e4af72889f62aebdc00d983
haris124421 <haris.fastian12@gmail.com>
2022-10-04 14:42:47 +0500

    added arabic language cases

--- cypress/integration/e2e/LanguageAR.js
+++ cypress/integration/e2e/LanguageAR.js
@@ -16,11 +16,8 @@ describe('Verifing arabic language of olx-lb', ()=> {
     it('it should visit the url', ()=> {
         cy.visitDomain(utility.authUsername, utility.authPassword)
     })
-<<<<<<< HEAD
     it('shoud switch to arabic', () => {
       cy.get('._39a8843c.cf485b3b').click()
       
     });
-=======
->>>>>>> 77a8e28fe33faf355b4944fe7ae365f464336afc
 })
