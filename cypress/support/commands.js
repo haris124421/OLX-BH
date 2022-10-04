c7208ac09b823e999abe1bee5c245c5845689260
haris124421 <haris.fastian12@gmail.com>
2022-10-04 12:26:53 +0500

    New Dev Branch

---
+++ cypress/support/commands.js
@@ -0,0 +1,50 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import LoginObjects from "../support/pageObjects/LoginObjects"
+import HomePage from "../support/pageObjects/HomePage"
+
+Cypress.Commands.add("visitDomain", (username, password) => {
+    cy.visit(Cypress.env('url'), {
+        auth: {
+          username: username,
+          password: password
+        }
+      })
+})
+
+Cypress.Commands.add('olxLogin', ($email, $password) => {
+
+  const loginObj = new LoginObjects();
+  const homePageObj = new HomePage();
+
+  homePageObj.loginbtn().click()
+  loginObj.loginWithEmail().click()
+  loginObj.enterEmail().type($email)
+  loginObj.nextBtn().click()
+  loginObj.enterPassword().type($password)
+  loginObj.clickLogin().click()
+})
---
+++ cypress/support/pageObjects/HomePage.js
@@ -0,0 +1,35 @@
+class HomePage{
+
+    olxLogo() {
+        return cy.get('.fb1b3abd.bcd9fd70')
+    }
+
+    loginbtn() {
+        return cy.get("._0db6bd2f._1b04dcc1")
+    }
+
+    UserIsLoggedIn(){
+        return cy.get('._0559c5ad')
+    }
+
+    FavoriteIcon(){
+        return cy.get('._3c2d02e2')
+    }
+    MarkedAsFavorite(){
+        return cy.get('[alt="favoriteIconSelected"]')
+    }
+
+    profileWindowArrow() {
+        return cy.get(".f5e6c6e9 > ._1075545d")
+    }
+
+    topbar() {
+        return cy.get("._1075545d._17fba712._96d4439a")
+    }
+
+    logut() {
+        return cy.get("div[aria-label$='Logout']")
+    }
+}
+
+export default HomePage;
