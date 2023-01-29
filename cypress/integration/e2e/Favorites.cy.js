798ea1d852238a82f83f6b8bd1ba807926d80dab
amir <amir.hamza@empglabs.com>
2023-01-29 15:01:39 +0500

    eg url change to bh

--- cypress/integration/e2e/Favorites.cy.js
+++ cypress/integration/e2e/Favorites.cy.js
@@ -8,34 +8,24 @@ describe('Favorites Cases', () => {
 
     
     let utility
-    
     before('Load Utility data', function() {
-      
         cy.fixture("utilityData.json").then((utilityData) => {
             utility = utilityData
             return cy.wrap(utility)
-        
         })
     })
 
     beforeEach('Visit Domain', () => {
-        
         cy.visitDomain(utility.authUsername, utility.authPassword)
-    
     })
 
     it('should open login page upon clicking fav icon without login', () => {
         
         homePageObj.FavoriteIcon()
         .should('be.visible')
-        .then(($favIcons) => {
-            
+        .then(($favIcons) => {  
             return Cypress._.sampleSize($favIcons.toArray(), 1)
-        
-        }).should(
-            
-            'have.length', 1
-        )
+        }).should('have.length', 1)
         .click().wait(4000)
         
         loginPageObj.loginPopup()
@@ -46,38 +36,33 @@ describe('Favorites Cases', () => {
     it('should mark as favorite', () => {
         
         cy.loginWithApi()
-        
         homePageObj.FavoriteIcon()
         .should('be.visible')
         .then(($favIcons) => {
             return Cypress._.sampleSize($favIcons.toArray(), 1)
         })
-        .should( 
-            'have.length', 1
-            ).click()
+        .should('have.length', 1).click()
         homePageObj.MarkedAsFavorite()
         .invoke('attr', 'class')
         .should('eq', '_1075545d _3c2d02e2 _840fd97c')
-        
-        
-   
     });
 
-    it.only('should unmark all favorites', ()=>{
+    it.only('should unmark all favorites', ()=> {
         cy.loginWithApi()
+        cy.reload();
         homePageObj.profileWindowArrow().click()
         homePageObj.myAds().click()
         cy.get('[href="/en/myfavorites"] > span').click()
         cy.get('body').then($body => {
             cy.wait(10000)
-            //const favads = $body.find("div._1075545d._3c2d02e2._840fd97c")
-            //cy.log(favads)
-            if ($body.find("div._1075545d._3c2d02e2._840fd97c").length > 0) {
-                cy.wrap($body.find("div._1075545d._3c2d02e2._840fd97c")).click({ multiple: true })
-                cy.log('All favorite ads have been unmarked.')
-            } else {
-              cy.log('No favorite ads available.')
-            }
+            const favads = cy.wrap($body).find("div._1075545d._3c2d02e2._840fd97c") > 0
+            cy.log(favads)
+            // if ($body.find("div._1075545d._3c2d02e2._840fd97c").length > 0) {
+            //     cy.wrap($body.find("div._1075545d._3c2d02e2._840fd97c")).click({ multiple: true })
+            //     cy.log('All favorite ads have been unmarked.')
+            // } else {
+            //   cy.log('No favorite ads available.')
+            // }
           })
     })
 
