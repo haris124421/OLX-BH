2fcfa431a7c933becbf861030681f66c0430d8ed
haris <haris.fastian12@gmail.com>
2023-02-02 03:10:54 +0500

    refactore folder structure

---
+++ cypress/integration/e2e/FrontEnd/LanguageAR.cy.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress"/>
+
+import HomePage from "../../../support/pageObjects/HomePage"
+
+
+const homePageobj = new HomePage();
+
+describe('Verifing arabic language of olx-lb', ()=> {
+    
+    let utility
+    before('Load Utility data', function() {
+      cy.fixture("utilityData.json").then((utilityData) => {
+        utility = utilityData
+      return cy.wrap(utility)
+      })
+    })
+
+    beforeEach('it should visit the url', ()=> {
+      if (Cypress.env('url')=='https://stage.olx-bh.run/en')
+      {
+        cy.visitDomain(utility.authUsername, utility.authPassword)
+      }
+      else{
+        cy.visit('https://olx.com.bh/en')
+      }
+        
+    })
+
+    it('shoud switch to arabic', () => {
+      
+      homePageobj.languageButton()
+      .click()
+      
+      homePageobj.languageButton()
+      .should('have.attr','aria-label')
+      .and('include','English')      
+    });
+})
