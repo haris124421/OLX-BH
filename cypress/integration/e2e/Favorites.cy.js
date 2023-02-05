eea080c508ecf5c31cf95576c2e46966aaec60f0
amir <amir.hamza@empglabs.com>
2023-02-05 17:38:42 +0500

    favorites spec re-maping is done - all fixtures are re-mapped. - same element re-mapped for jquery, removed extra jquery locators from fixtures.

--- cypress/integration/e2e/Favorites.cy.js
+++ cypress/integration/e2e/Favorites.cy.js
@@ -7,38 +7,54 @@ const loginPageObj = new LoginObjects();
 describe('Favorites Cases', () => {
 
     
-    let utility;
-    let favorites;
-    let login;
-    let home;
-    before('Load Utility data', function() {
-        cy.fixture("utilityData.json").then((utilityData) => {
-            utility = utilityData
-            return cy.wrap(utility)
-        })
-        cy.fixture("page_objects/Favorites.json").then(($favorites) => {
-            favorites = $favorites
-            return cy.wrap(favorites)
-        })
-        cy.fixture("page_objects/login.json").then(($login) => {
-            login = $login
-            return cy.wrap(login)
-        })
-        cy.fixture("page_objects/home.json").then(($home) => {
-            home = $home
-            return cy.wrap(home)
-        })
-        
-    })
-
+    // let utility;
+    // let favorites;
+    // let login;
+    // let home;
+    // before('Load Utility data', function() {
+    //     cy.fixture("utilityData.json").then(($utilityData) => {
+    //         utility = $utilityData
+    //         return cy.wrap(utility)
+    //     })
+    //     cy.fixture("page_objects/Favorites.json").then(($favorites) => {
+    //         favorites = $favorites
+    //         return cy.wrap(favorites)
+    //     })
+    //     cy.fixture("page_objects/login.json").then(($login) => {
+    //         login = $login
+    //         return cy.wrap(login)
+    //     })
+    //     cy.fixture("page_objects/home.json").then(($home) => {
+    //         home = $home
+    //         return cy.wrap(home)
+    //     })  
+    // })
+    let fixtures;
+    beforeEach(() => {
+        cy.fixture('utilityData.json')
+        .then($utilityData => {
+            cy.fixture('page_objects/Favorites.json')
+            .then($favorites => {
+                cy.fixture('page_objects/login.json')
+                .then($login => {
+                    cy.fixture('page_objects/home.json')
+                    .then($home => {
+                        fixtures = {$utilityData, $favorites, $login, $home}
+                        return cy.wrap(fixtures)
+                    })
+                })
+            });
+        });
+    });
+    
     beforeEach('Visit Domain', () => {
-        cy.visitDomain(utility.authUsername, utility.authPassword)
+        cy.visitDomain(fixtures.$utilityData.authUsername, fixtures.$utilityData.authPassword)
     })
 
     it('should open login page upon clicking fav icon without login', () => {
         
         // homePageObj.FavoriteIcon()
-        cy.get(favorites.FavoriteIcon)
+        cy.get(fixtures.$favorites.FavoriteIcon)
         .should('be.visible')
         .then(($favIcons) => {  
             return Cypress._.sampleSize($favIcons.toArray(), 1)
@@ -46,7 +62,7 @@ describe('Favorites Cases', () => {
         .click().wait(10000)
         
         // loginPageObj.loginPopup()
-        cy.get(login.login_popup)
+        cy.get(fixtures.$login.login_popup)
         .should('be.visible')
 
     })
@@ -55,7 +71,7 @@ describe('Favorites Cases', () => {
         
         cy.loginWithApi()
         // homePageObj.FavoriteIcon()
-        cy.get(favorites.FavoriteIcon)
+        cy.get(fixtures.$favorites.FavoriteIcon)
         .should('be.visible')
         .then(($favIcons) => {
             return Cypress._.sampleSize($favIcons.toArray(), 1)
@@ -67,33 +83,45 @@ describe('Favorites Cases', () => {
             }
         }).click()
         // homePageObj.MarkedAsFavorite()
-        cy.get(favorites.MarkedAsFavorite)
+        var withDots = fixtures.$favorites.MarkedAsFavorite;
+        var dotsRemoved = withDots.replace(/\./g, ' ');
+        cy.log(dotsRemoved);
+
+        var newMarkasFav = dotsRemoved.slice(1);
+        cy.log(newMarkasFav);
+
+        cy.get(fixtures.$favorites.MarkedAsFavorite)
         .invoke('attr', 'class')
-        .should('eq', favorites.MarkedAsFavorite)
+        .should('eq', newMarkasFav)
     });
 
     it('should unmark all favorites', ()=>{
         cy.loginWithApi()
         cy.reload();
         // homePageObj.profileWindowArrow()
-        cy.get(home.profileWindowArrow).click()
+        cy.get(fixtures.$home.profileWindowArrow).click()
         // homePageObj.myAds()
-        cy.get(home.myAds).click()
-        cy.get(home.favorites).click()
+        cy.get(fixtures.$home.myAds).click()
+        cy.get(fixtures.$home.favorites).click()
         cy.wait(6000)
         cy.get('body').then($body => {
             cy.wait(10000)
             //const favads = $body.find("div._1075545d._3c2d02e2._840fd97c")
             //cy.log(favads)
-            if ($body.find(home.favoritesAds).length > 0) {
-                cy.wrap($body.find(home.favoritesAds)).click({ multiple: true })
+            if ($body.find(fixtures.$home.favoritesAds).length > 0) {
+                cy.wrap($body.find(fixtures.$home.favoritesAds)).each(($el, index) => {
+                    cy.get($el).click().wait(3000)
+                })
+                // cy.reload()
                 cy.log('All favorite ads have been unmarked.')
-                .should("eq", "All favorite ads have been unmarked.")
+                cy.wait(5000)
+                cy.get(fixtures.$favorites.NoFavAds)
+                .should('have.text','No favorites yet.')
             } else {
-              cy.log('No favorite ads available.')
-              .should('eq', "No favorite ads available.")
+              cy.log('Already No favorite ads available.')
+              cy.get(fixtures.$favorites.NoFavAds)
+              .should('have.text','No favorites yet.')
             }
-          })
+        })
     })
-
 });
